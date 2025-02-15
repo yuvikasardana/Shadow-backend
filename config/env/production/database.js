c4011818f9da60a4c2823879c2737770986a1230
Yuvika <yuvikasardana@gmail.com>
2025-02-15 17:00:31 +0530

    more production changes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,19 @@
+import { parse } from 'pg-connection-string';
+const config = parse(process.env.DATABASE_URL);
+
 export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'), 
-        port: env.int('DATABASE_PORT'), 
-        database: env('DATABASE_NAME'), 
-        user: env('DATABASE_USERNAME'), 
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
