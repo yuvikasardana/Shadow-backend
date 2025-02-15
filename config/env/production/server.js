2448471e682dbe454b60a1aae04cf11cd6bd3fe8
Yuvika <yuvikasardana@gmail.com>
2025-02-15 22:22:59 +0530

    trying more

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-export default ({ env }) => ({
-    proxy: true,
-    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
-    app: { 
-         keys: env.array('APP_KEYS')
-       },
-   });
